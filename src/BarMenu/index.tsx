161b2230b470d3787d112ede95885ea865c3cc5e
vlckomi <vlckova.michaela5@gmail.com>
2025-07-04 01:53:55 +0200

    Style + UX polish

--- src/BarMenu/index.tsx
+++ src/BarMenu/index.tsx
@@ -14,104 +14,220 @@ import { useState } from "react";
 import { ErrorAlert } from "../ErrorAlert";
 import { useStore } from "../store/db";
 import { useApplication } from "../useApplication";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+type PaymentState =
+  | ["cash"]
+  | ["card"]
+  | ["tab"]
+  | ["tab", id: number]
+  | undefined;
 
 export const BarMenu = () => {
   const { priceData, colorIndexes, order, orderSubmitting, actions } =
     useApplication();
 
+  const store = useStore();
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["tabs"],
+    queryFn: () => store.readAllTabs(),
+  });
+
+  const [paymentState, setPaymentState] = useState<PaymentState>();
+
+  const categorisedPriceData =
+    priceData.data && groupBy(Object.values(priceData.data), (x) => x.category);
+
   return (
     <>
-      <Stack gap={2} sx={{ flexGrow: 6 }}>
-        <Typography variant="h4">Nabídka</Typography>
+      <Stack gap={2} sx={{ flexGrow: 1 }}>
+        <Typography variant="h4" sx={{ marginTop: 2 }}>
+          Nabídka
+        </Typography>
         <ErrorAlert e={priceData.error} />
         {priceData.isLoading && <LinearProgress />}
-        {priceData.data &&
-          Object.values(priceData.data).map((item, i) => (
-            <Card
-              key={i}
-              sx={{
-                backgroundColor: COLORS[colorIndexes!.indexOf(item.category)],
-                color: "black",
-              }}
-            >
-              <CardActionArea
-                sx={{ p: 1.5 }}
-                onClick={() => actions.addToOrder(item.id)}
-              >
-                <Stack direction="row" justifyContent="space-between">
-                  <Typography variant="body1">{item.name}</Typography>
-                  <Typography variant="body1">{item.price} Kč</Typography>
-                </Stack>
-              </CardActionArea>
-            </Card>
+        {categorisedPriceData &&
+          Object.entries(categorisedPriceData).map(([key, values]) => (
+            <Stack key={key} gap={1}>
+              <Typography variant="h5">{key}</Typography>
+              <Grid container spacing={1}>
+                {values.map((item, i) => (
+                  <Grid size={4}>
+                    <Card
+                      key={i}
+                      sx={{
+                        backgroundColor:
+                          COLORS[colorIndexes!.indexOf(item.category)],
+                        color: "black",
+                      }}
+                    >
+                      <CardActionArea
+                        sx={{ p: 1.5 }}
+                        onClick={() => actions.addToOrder(item.id)}
+                      >
+                        <Stack
+                          direction="column"
+                          justifyContent="space-between"
+                        >
+                          <Typography
+                            variant="body1"
+                            sx={{ fontWeight: "bold" }}
+                          >
+                            {item.name}
+                          </Typography>
+                          <Typography variant="body1">
+                            {item.price} Kč
+                          </Typography>
+                        </Stack>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Stack>
           ))}
       </Stack>
       <Stack
         gap={2}
-        sx={{ flexGrow: 4 }}
+        sx={{ flexGrow: 0, flexShrink: 0 }}
         pr={2}
-        height="99vh"
+        height="96vh"
+        maxHeight="96vh"
         position="sticky"
         top={0}
+        justifyContent="space-between"
+        overflow="auto"
       >
-        <Typography variant="h4">Objednávka</Typography>
-        {priceData.data &&
-          order.map((item, i) => (
-            <Card key={i} sx={{ p: 1 }}>
-              <Stack direction="row" gap={1} alignItems="center">
-                <Stack>
-                  <Typography variant="body1">
-                    <strong>{item.amount} ×</strong>
-                  </Typography>
-                  <Typography variant="body1">
-                    {priceData.data[item.itemId].name}
-                  </Typography>
-                </Stack>
-                <Button
-                  onClick={() => actions.removeFromOrder(item.itemId)}
-                  size="small"
-                >
-                  −
-                </Button>
-              </Stack>
-            </Card>
-          ))}
+        <Stack gap={2} sx={{ width: "300px" }}>
+          <Typography variant="h4" sx={{ pt: 2 }}>
+            Objednávka
+          </Typography>
+          <Stack gap={1}>
+            {priceData.data &&
+              order.map((item, i) => (
+                <Card key={i}>
+                  <Stack
+                    direction="row"
+                    gap={3}
+                    alignItems="stretch"
+                    justifyContent="space-between"
+                  >
+                    <Stack direction="row" gap={1} py={1} px={2}>
+                      <Typography variant="body1">
+                        <strong>{item.amount} ×</strong>
+                      </Typography>
+                      <Typography variant="body1">
+                        {priceData.data[item.itemId].name}
+                      </Typography>
+                    </Stack>
 
-        {!!order.length && (
-          <Stack direction="row" justifyContent="space-between">
-            <Typography variant="h5">Celkem</Typography>
-            <Typography variant="h5">
-              {order.reduce(
-                (acc, x) => acc + priceData.data![x.itemId].price * x.amount,
-                0
-              )}{" "}
-              Kč
-            </Typography>
+                    <IconButton
+                      onClick={() => actions.removeFromOrder(item.itemId)}
+                      size="small"
+                      sx={{
+                        background: "rgba(255, 255, 255, .1)",
+                        color: "red",
+                        borderRadius: 0,
+                        px: 1,
+                      }}
+                    >
+                      <RemoveIcon />
+                    </IconButton>
+                  </Stack>
+                </Card>
+              ))}
           </Stack>
-        )}
-
-        <Stack direction="row" gap={1}>
-          <Button
-            variant="contained"
-            onClick={() => actions.submitOrder("cash")}
-            disabled={!order.length || orderSubmitting}
-          >
-            Hotově
-          </Button>
-          <Button
-            variant="contained"
-            onClick={() => actions.submitOrder("card")}
-            disabled={!order.length || orderSubmitting}
-          >
-            Kartou
-          </Button>
-          <TabPayButton
-            disabled={!order.length || orderSubmitting}
-            onClick={(id) => actions.submitOrder(`tab_${id}`)}
-          />
         </Stack>
-        {orderSubmitting && <LinearProgress />}
+
+        <Stack gap={2}>
+          {!!order.length && (
+            <Stack direction="row" justifyContent="space-between">
+              <Typography variant="h5">Celkem</Typography>
+              <Typography variant="h5">
+                {order.reduce(
+                  (acc, x) => acc + priceData.data![x.itemId].price * x.amount,
+                  0
+                )}{" "}
+                Kč
+              </Typography>
+            </Stack>
+          )}
+          <ToggleButtonGroup
+            color="primary"
+            value={paymentState?.[0]}
+            exclusive
+            onChange={(_, v) => setPaymentState([v])}
+          >
+            <ToggleButton value="cash">Hotově</ToggleButton>
+            <ToggleButton value="card">Kartou</ToggleButton>
+            <ToggleButton value="tab">
+              {data &&
+              paymentState &&
+              paymentState[0] === "tab" &&
+              paymentState[1] !== undefined
+                ? data.find((t) => t.id === paymentState[1])?.name
+                : "Ůčet"}
+            </ToggleButton>
+          </ToggleButtonGroup>
+          <Button
+            fullWidth
+            variant="contained"
+            disabled={
+              order.length === 0 ||
+              !paymentState ||
+              (paymentState[0] === "tab" ? paymentState.length === 1 : false)
+            }
+            onClick={() => {
+              if (!paymentState) return;
+
+              actions.submitOrder(
+                paymentState[0] === "tab"
+                  ? `tab_${paymentState[1]!}`
+                  : paymentState[0]
+              );
+              setPaymentState(undefined);
+            }}
+          >
+            Odeslat
+          </Button>
+          {orderSubmitting && <LinearProgress />}
+        </Stack>
       </Stack>
+      <Dialog
+        open={
+          !!paymentState &&
+          paymentState.length === 1 &&
+          paymentState[0] === "tab"
+        }
+        onClose={() => setPaymentState(undefined)}
+        maxWidth={false}
+      >
+        <DialogTitle>Vyber ůčet:</DialogTitle>
+
+        <DialogContent sx={{ minWidth: "30vw" }}>
+          <ErrorAlert e={error} />
+          {isLoading && <LinearProgress />}
+          {data && (
+            <List>
+              {data.map((t) => (
+                <ListItemButton
+                  key={t.id}
+                  onClick={() => {
+                    setPaymentState(["tab", t.id]);
+                  }}
+                >
+                  {t.name}
+                </ListItemButton>
+              ))}
+            </List>
+          )}
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
@@ -124,54 +240,21 @@ const COLORS = [
   "oklch(71.4% 0.203 305.504)",
 ];
 
-const TabPayButton = ({
-  disabled,
-  onClick,
-}: {
-  disabled: boolean;
-  onClick: (id: number) => void;
-}) => {
-  const store = useStore();
+const groupBy = <T, U extends string>(
+  array: T[],
+  keySelector: (itm: T) => U
+) => {
+  const result: Record<U, T[]> = {} as never;
 
-  const [open, setOpen] = useState(false);
+  for (const itm of array) {
+    const key = keySelector(itm);
 
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["tabs"],
-    queryFn: () => store.readAllTabs(),
-  });
+    if (!result[key]) {
+      result[key] = [];
+    }
 
-  return (
-    <>
-      <Button
-        variant="contained"
-        disabled={disabled}
-        onClick={() => setOpen(true)}
-      >
-        Ůčet
-      </Button>
-      <Dialog open={open} onClose={() => setOpen(false)} maxWidth={false}>
-        <DialogTitle>Vyber ůčet:</DialogTitle>
+    result[key].push(itm);
+  }
 
-        <DialogContent sx={{ minWidth: "30vw" }}>
-          <ErrorAlert e={error} />
-          {isLoading && <LinearProgress />}
-          {data && (
-            <List>
-              {data.map((t) => (
-                <ListItemButton
-                  key={t.id}
-                  onClick={() => {
-                    onClick(t.id);
-                    setOpen(false);
-                  }}
-                >
-                  {t.name}
-                </ListItemButton>
-              ))}
-            </List>
-          )}
-        </DialogContent>
-      </Dialog>
-    </>
-  );
+  return result;
 };
